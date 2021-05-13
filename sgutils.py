f20189a9a375a17a8395d184d12b8774639af809
mdthielen <mark.thielen@dreamview.com>
2021-05-13 12:26:04 -0700

    add timing tset march 2021

--- sgutils.py
+++ sgutils.py
@@ -8,7 +8,8 @@ import datetime
 import json
 import os
 
-source_path = r'D:\shotgun\source_files'
+# source_path = r'D:\shotgun\source_files'
+source_path = r'D:\shotgun\202103'
 fix_path = r'D:\shotgun\fix_phage'
 
 def process(sg):
@@ -17,7 +18,11 @@ def process(sg):
     Args:
         sg (:obj:): shotgun instance
     """
-    print('testing from process...')
+    print('\nSearching for infected files...')
+    total_time = None
+    total_scan_time = None
+    total_file_size = 0
+
     tags = [{'type': 'Tag', 'id': 4379}]
     any_filters = [
         ['filename', 'ends_with', '.mb'],
@@ -25,28 +30,45 @@ def process(sg):
 
     filters = [
         # ['id', 'in', [1717073, 1659356, 1073338]],
-        ['created_at', 'greater_than', datetime.datetime(2021, 4, 1)],
-        ['tags', 'not_in', tags],
+        # ['created_at', 'greater_than', datetime.datetime(2021, 4, 16)],
+        ['created_at', 'in_calendar_month', -2],
+        # ['tags', 'not_in', tags],
         {'filter_operator': 'any',
                             'filters': any_filters}
     ]
     fields = list(sg.schema_field_read('Attachment').keys())
-    # fields = ['filename', 'created_at', 'attachment_links', 'project']
     attachments = sg.find('Attachment', filters, fields)
 
     source_files = {}
     bad = []
     not_bad = []
+    time = None
+
     for attachment in attachments:
         print('Processing: {}'.format(attachment.get('filename')))
-        path = _download_file(sg, attachment)
+        (path, time, file_size) = _download_file(sg, attachment)
+        if not time:
+            continue
+
+        if not total_time:
+            total_time = time
+        else:
+            total_time = total_time + time
+        total_file_size = total_file_size + file_size
 
         if not path:
             continue
 
+        start_time = datetime.datetime.now()
         suspect = open(path, 'rb').read()
         if b'phage' in suspect:
-            print('Bad: {}'.format(path))
+            end_time = datetime.datetime.now()
+            if not total_scan_time:
+                total_scan_time = end_time - start_time
+            else:
+                total_scan_time = total_scan_time + end_time - start_time
+
+            print('****  Bad: {} - Time for search: {}  ****'.format(path, (end_time-start_time)))
             bad.append(path)
             attachment['source_file'] = path
             attachment['source_file_id'] = attachment['id']
@@ -58,11 +80,19 @@ def process(sg):
                 f.write(json.dumps(source_files, indent=4))
             sg.update("Attachment", attachment['id'], {'tags': tags})
         else:
+            end_time = datetime.datetime.now()
+            print('NOT Bad: {} - Time for search: {}'.format(path, (end_time-start_time)))
             not_bad.append(path)
             try:
                 os.remove(path)
             except IOError as e:
                 print(e)
+                if '[Errno 28] No space left on device' in e:
+                    return
+
+    print('\nTotal Size:          {} bytes'.format(total_file_size))
+    print('Total Scan Time:     {}'.format(total_scan_time))
+    print('Total Download Time: {}\n'.format(total_time))
 
 def _download_file(sg, attachment):
     date = attachment['created_at']
@@ -72,11 +102,16 @@ def _download_file(sg, attachment):
 
     # attachment = {'type': 'Attachment', 'id': attachment_id }
     if os.path.isfile(source_path_file):
-        return
+        return (None, None, 0)
 
+    start_time = datetime.datetime.now()
     sg.download_attachment(attachment, file_path=source_path_file)
-
-    return source_path_file
+    end_time = datetime.datetime.now()
+    file_size = os.path.getsize(source_path_file)
+    total_time = end_time - start_time
+    print('Download time:  {}\t\t\tFile Size:  {}'.format(total_time, file_size))
+    
+    return (source_path_file, total_time, file_size)
 
 
 if __name__ == '__main__':
